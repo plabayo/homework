d35b0d8a8a6ae1f3c632e7cdd9aeec2fd8a78382
Glen De Cauwsemaecker <contact@glendc.com>
2025-09-21 19:58:50 +0200

    add rama+fly info to response headers
    as we are now behind a reverse fly.io
    proxy, so need other means to get info out

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,2 +1,53 @@
-mod http;
-pub use http::load_http_service;
+use std::{convert::Infallible, path::Path};
+
+use rama::{
+    Context, Layer as _, Service,
+    error::{BoxError, ErrorContext as _, OpaqueError},
+    http::{
+        Body, BodyExtractExt, HeaderName, HeaderValue, Request, Response, StatusCode, header,
+        headers::{Authorization, ContentType},
+        layer::{
+            cors, map_response_body::MapResponseBodyLayer,
+            required_header::AddRequiredResponseHeadersLayer, set_header::SetResponseHeaderLayer,
+            trace::TraceLayer,
+        },
+        service::{
+            client::HttpClientExt as _,
+            web::{
+                Router,
+                response::{Headers, IntoResponse as _},
+            },
+        },
+    },
+    net::http::RequestContext,
+    service::service_fn,
+    telemetry::tracing,
+};
+
+mod legacy;
+
+pub async fn load_http_service(
+    path: &Path,
+) -> impl Service<Request, Response = Response, Error = Infallible> {
+    let app = Router::new().sub("/", legacy::service(path));
+
+    (
+        MapResponseBodyLayer::new(Body::new),
+        TraceLayer::new_for_http(),
+        AddRequiredResponseHeadersLayer::default(),
+        SetResponseHeaderLayer::overriding(
+            HeaderName::from_static("x-powered-by"),
+            HeaderValue::from_static(const_format::formatcp!(
+                "{}/{}",
+                rama::utils::info::NAME,
+                rama::utils::info::VERSION
+            )),
+        ),
+        SetResponseHeaderLayer::overriding(
+            HeaderName::from_static("x-sponsored-by"),
+            HeaderValue::from_static("fly.io"),
+        ),
+        cors::CorsLayer::permissive(),
+    )
+        .into_layer(app)
+}
